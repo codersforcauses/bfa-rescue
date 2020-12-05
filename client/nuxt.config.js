76a9b68d20a23a00918535218c1e5085e64628f0
b053040009@gmail.com <b053040009@gmail.com>
2020-12-06 03:19:50 +0800

    modified colour palette

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,11 +1,9 @@
-import colors from 'vuetify/es5/util/colors'
-
 export default {
   target: 'static',
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: '%s - client',
-    title: 'client',
+    title: 'BFA Rescue',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -46,16 +44,15 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
+        light: {
+          primary: '#4AB0C5',
+          secondary: '#FA7921',
+          accent: '#000000',
+          info: '#C3DFDF',
+          warning: '#D3BE70',
+          error: '#EF7674',
+          success: '#8BE8CB',
         },
       },
     },
