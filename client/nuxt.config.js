d16bb16c7b0c665a2a3640102dbca970aa09bd6a
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2020-12-07 15:49:33 +0800

    add page lang

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -2,6 +2,9 @@ export default {
   target: 'static',
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
+    htmlAttrs: {
+      lang: 'en-AU',
+    },
     titleTemplate: '%s - client',
     title: 'BFA Rescue',
     meta: [
