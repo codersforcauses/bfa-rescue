6410bd4d2a2444cef18c3e4cfc3c92314e61c479
lisaguo-00 <56644642+lisaguo-00@users.noreply.github.com>
2020-12-12 15:38:00 +0800

    HeaderV1

---
+++ client/components/health-nav/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container background-color="light blue" color="white">
+    <h1>
+      <p class="text-center">Health & Nutrition</p>
+    </h1>
+    <h3>
+      <p class="text-center">
+        A collection of helpful insights to keeping your pet dog healthy and
+        happy
+      </p>
+    </h3>
+  </v-container>
+</template>
---
+++ client/pages/health-nav.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from '~/components/health-nav/Header.vue'
+
+export default {
+  components: {
+    Header,
+  },
+}
+</script>
