e627e6e0ae36acab184c163b14054dc75cc0e966
ivy0305 <64839317+ivy0305@users.noreply.github.com>
2020-12-12 15:39:12 +0800

    Cards for health nav page

---
+++ client/components/health-nav/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card class="mx-auto">
+    <v-img :src="imagelink" height="250px"></v-img>
+    <v-card-title>{{ cardtitle }}</v-card-title>
+    <v-card-text>{{ carddescription }}</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'HealthNavCard',
+  props: {
+    imagelink: {
+      type: String,
+      default: '',
+    },
+    cardtitle: {
+      type: String,
+      default: '',
+    },
+    carddescription: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
