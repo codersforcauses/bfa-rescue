fb31c1165dd36f9f90f71e745d7cff6475352a26
bpgoodwin <bpgoodwin@protonmail.ch>
2020-12-14 21:50:33 +0800

    Duplicated content + header

--- client/components/Hero.vue
+++ client/components/Hero.vue
@@ -2,17 +2,21 @@
   <div class="hero">
     <div class="container">
       <div class="hero-content">
-        <h1 class="hero-title">
-          Provide a service that helps us with our rescue work
-        </h1>
-        <p>
-          Best Friends Animal Rescue is run by a group of volunteers, so if you
-          you can provide services such as training, grooming, photography,
-          construction, etc. Please reach out to us. We need all the help we can
-          get with our rescue operations.
-        </p>
-        <div class="hero-btn mt-5">
-          <v-btn class="btn">Contact Us</v-btn>
+        <div class="row justify-content-center align-items-">
+          <div class="col-md-10">
+            <h1 class="hero-title">
+              Provide a service that helps us with our rescue work
+            </h1>
+            <p>
+              Best Friends Animal Rescue is run by a group of volunteers, so if
+              you can provide services such as training, grooming, photography,
+              construction, etc. Please reach out to us. We need all the help we
+              can get with our rescue operations.
+            </p>
+            <div class="hero-btn mt-5">
+              <v-btn class="btn">Contact Us</v-btn>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -27,16 +31,19 @@ export default {
 
 <style scoped lang="scss">
 .hero {
-  background-color: bisque;
+  background-image: url('https://secureservercdn.net/160.153.138.177/wzj.f59.myftpupload.com/wp-content/uploads/2015/10/white-dog-tongue.png');
+  background-position: right;
+  background-size: 90%;
+  background-color: rgb(255, 255, 255);
   text-align: center;
   width: 100vw;
   position: relative;
   margin-left: -50vw;
-  height: 300px;
+  height: 400px;
   margin-bottom: 50px;
   margin-top: -15px;
   left: 50%;
-  padding-top: 30px;
+  padding-top: 70px;
 }
 .hero-title {
   padding-bottom: 30px;
--- client/pages/supporters.vue
+++ client/pages/supporters.vue
@@ -1,212 +1,214 @@
 <template>
-  <div class="supporter-cards">
+  <div class="container">
     <hero />
-    <v-row justify="center" align="center">
-      <v-col cols="12" sm="6" md="4">
-        <v-card>
-          <v-img
-            :src="
-              require('@/assets/sponsors/energy-efficient-communities-program.png')
-            "
-            alt="energy-efficient-communities-program"
-            height="200px"
-          ></v-img>
-          <v-card-title>Energy Efficient Communities</v-card-title>
-          <v-card-text>
-            The new sanctuary is Solar Powered! Thanks, EECP!
-          </v-card-text>
-          <v-card-actions>
-            <v-btn
-              href="https://www.energy.gov.au/government-priorities/energy-programs/energy-efficient-communities-program"
-              text
-              rel="noreferrer"
+    <div class="full-width">
+      <v-row justify="center" align="center" id="regular" class="top">
+        <v-col cols="12" sm="6" md="4">
+          <v-card>
+            <v-img
+              :src="
+                require('@/assets/sponsors/energy-efficient-communities-program.png')
+              "
+              alt="energy-efficient-communities-program"
+              height="200px"
+            ></v-img>
+            <v-card-title>Energy Efficient Communities</v-card-title>
+            <v-card-text>
+              The new sanctuary is Solar Powered! Thanks, EECP!
+            </v-card-text>
+            <v-card-actions>
+              <v-btn
+                href="https://www.energy.gov.au/government-priorities/energy-programs/energy-efficient-communities-program"
+                text
+                rel="noreferrer"
+              >
+                Visit
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="6" md="4">
+          <v-card>
+            <v-img
+              :src="require('@/assets/sponsors/syds-building.jpg')"
+              height="200px"
+              alt="syds-building"
             >
-              Visit
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col cols="12" sm="6" md="4">
-        <v-card>
-          <v-img
-            :src="require('@/assets/sponsors/syds-building.jpg')"
-            height="200px"
-            alt="syds-building"
-          >
-          </v-img>
-          <v-card-title>Syd's Building Maintainance</v-card-title>
-          <v-card-text>Paul Barker, you are our HERO!</v-card-text>
-          <v-card-actions>
-            <v-btn
-              href="https://www.facebook.com/SydsBuildingMaintenance/"
-              text
-              rel="noreferrer"
+            </v-img>
+            <v-card-title>Syd's Building Maintainance</v-card-title>
+            <v-card-text>Paul Barker, you are our HERO!</v-card-text>
+            <v-card-actions>
+              <v-btn
+                href="https://www.facebook.com/SydsBuildingMaintenance/"
+                text
+                rel="noreferrer"
+              >
+                Visit
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="6" md="4">
+          <v-card>
+            <v-img
+              :src="require('@/assets/sponsors/southern-raw-pet.jpg')"
+              height="200px"
+              alt="southern-raw-pet-meats"
             >
-              Visit
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col cols="12" sm="6" md="4">
-        <v-card>
-          <v-img
-            :src="require('@/assets/sponsors/southern-raw-pet.jpg')"
-            height="200px"
-            alt="southern-raw-pet-meats"
-          >
-          </v-img>
-          <v-card-title>Southern Raw Pet Meats</v-card-title>
-          <v-card-text> Thanks to SRPM for donations of food!</v-card-text>
-          <v-card-actions>
-            <v-btn
-              href="https://www.southernrawpetmeats.com.au/"
-              text
-              rel="noreferrer"
+            </v-img>
+            <v-card-title>Southern Raw Pet Meats</v-card-title>
+            <v-card-text> Thanks to SRPM for donations of food!</v-card-text>
+            <v-card-actions>
+              <v-btn
+                href="https://www.southernrawpetmeats.com.au/"
+                text
+                rel="noreferrer"
+              >
+                Visit
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row justify="center" align="center" id="regular">
+        <v-col cols="12" sm="6" md="4">
+          <v-card>
+            <v-img
+              :src="require('@/assets/sponsors/lotterywest.jpg')"
+              height="200px"
+              alt="lotterywest"
             >
-              Visit
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row justify="center" align="center">
-      <v-col cols="12" sm="6" md="4">
-        <v-card>
-          <v-img
-            :src="require('@/assets/sponsors/lotterywest.jpg')"
-            height="200px"
-            alt="lotterywest"
-          >
-          </v-img>
-          <v-card-title>LotteryWest</v-card-title>
-          <v-card-text>
-            Thank you to LotteryWest for a grant to furnish the new sanctuary
-            building with flooring and whitegoods! You can visit their website
-            at the link below!
-          </v-card-text>
-          <v-card-actions>
-            <v-btn
-              href="https://www.lotterywest.wa.gov.au/"
-              rel="noreferrer"
-              text
+            </v-img>
+            <v-card-title>LotteryWest</v-card-title>
+            <v-card-text>
+              Thank you to LotteryWest for a grant to furnish the new sanctuary
+              building with flooring and whitegoods! You can visit their website
+              at the link below!
+            </v-card-text>
+            <v-card-actions>
+              <v-btn
+                href="https://www.lotterywest.wa.gov.au/"
+                rel="noreferrer"
+                text
+              >
+                Visit
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="6" md="4">
+          <v-card>
+            <v-img
+              :src="require('@/assets/sponsors/dog-centric.jpg')"
+              height="200px"
+              alt="DogCentric"
             >
-              Visit
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col cols="12" sm="6" md="4">
-        <v-card>
-          <v-img
-            :src="require('@/assets/sponsors/dog-centric.jpg')"
-            height="200px"
-            alt="DogCentric"
-          >
-          </v-img>
-          <v-card-title>DogCentric</v-card-title>
-          <v-card-text>
-            DogCentric's goal is to improve the relationship between people and
-            their dogs. If you are a BFA Rescue foster carer you’ll get a 25%
-            discount.
-          </v-card-text>
-          <v-card-actions>
-            <v-btn
-              href="https://www.facebook.com/DogCentricPerth"
-              rel="noreferrer"
-              text
+            </v-img>
+            <v-card-title>DogCentric</v-card-title>
+            <v-card-text>
+              DogCentric's goal is to improve the relationship between people
+              and their dogs. If you are a BFA Rescue foster carer you get a
+              discount.
+            </v-card-text>
+            <v-card-actions>
+              <v-btn
+                href="https://www.facebook.com/DogCentricPerth"
+                rel="noreferrer"
+                text
+              >
+                Visit
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="6" md="4">
+          <v-card>
+            <v-img
+              :src="require('@/assets/sponsors/shutter-paws.jpg')"
+              height="200px"
+              alt="shutterpaws"
             >
-              Visit
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col cols="12" sm="6" md="4">
-        <v-card>
-          <v-img
-            :src="require('@/assets/sponsors/shutter-paws.jpg')"
-            height="200px"
-            alt="shutterpaws"
-          >
-          </v-img>
-          <v-card-title>Shutter Paws</v-card-title>
-          <v-card-text>
-            Shutter Paws are photographers donating their time helping foster
-            carers, rescue groups and shelters to get the best possible photos
-            to help with adoption.
-          </v-card-text>
-          <v-card-actions>
-            <v-btn
-              href="https://www.facebook.com/shutterpawsPerth"
-              rel="noreferrer"
-              text
+            </v-img>
+            <v-card-title>Shutter Paws</v-card-title>
+            <v-card-text>
+              Shutter Paws are photographers donating their time helping foster
+              carers, rescue groups and shelters to get the best possible photos
+              to help with adoption.
+            </v-card-text>
+            <v-card-actions>
+              <v-btn
+                href="https://www.facebook.com/shutterpawsPerth"
+                rel="noreferrer"
+                text
+              >
+                Visit
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row justify="center" align="center" id="regular" class="bottom">
+        <v-col cols="12" sm="6" md="4">
+          <v-card>
+            <v-img
+              :src="require('@/assets/sponsors/michelles-dog-grooming.jpg')"
+              height="200px"
+              alt="michelles-dog-grooming"
             >
-              Visit
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row justify="center" align="center">
-      <v-col cols="12" sm="6" md="4">
-        <v-card>
-          <v-img
-            :src="require('@/assets/sponsors/michelles-dog-grooming.jpg')"
-            height="200px"
-            alt="michelles-dog-grooming"
-          >
-          </v-img>
-          <v-card-title>Michelle's Dog Grooming</v-card-title>
-          <v-card-text>
-            My service is like no other as I always consider your dogs wellbeing
-            and safety first and I treat them as if they were my own.<br /><br />
-            Michelle Kefalinos - 0439 781 305
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="12" sm="6" md="4">
-        <v-card>
-          <v-img
-            :src="require('@/assets/sponsors/nola-criddle-foundation.jpg')"
-            height="200px"
-            alt="nola-criddle-foundation"
-          >
-          </v-img>
-          <v-card-title>Nola Criddle Foundations</v-card-title>
-          <v-card-text>
-            A big thanks to the Nola Criddle Foundation for your continued
-            sponsorship. If we didn’t have your sponsorship for the last 4 years
-            we wouldn’t have been able to start building our senior and special
-            retirement home for dogs.
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="12" sm="6" md="4">
-        <v-card>
-          <v-img
-            :src="
-              require('@/assets/sponsors/kevin-mcnulty-dog-behaviourist.jpg')
-            "
-            height="200px"
-            alt="kevin-mcnulty-dog-behaviourist"
-          >
-          </v-img>
-          <v-card-title>Kevin McNulty Dog Behaviourist</v-card-title>
-          <v-card-text>
-            Kevin provides gentle, professional training to dogs in the Perth
-            Metropolitan area. Call at 0416 791 195
-          </v-card-text>
-          <v-card-actions>
-            <v-btn
-              href="https://www.facebook.com/k9trainingandcommunication/"
-              text
-              rel="noreferrer"
+            </v-img>
+            <v-card-title>Michelle's Dog Grooming</v-card-title>
+            <v-card-text>
+              My service is like no other as I always consider your dogs
+              wellbeing and safety first and I treat them as if they own.<br /><br />
+              Michelle Kefalinos - 0439 781 305
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="6" md="4">
+          <v-card>
+            <v-img
+              :src="require('@/assets/sponsors/nola-criddle-foundation.jpg')"
+              height="200px"
+              alt="nola-criddle-foundation"
             >
-              Visit
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
+            </v-img>
+            <v-card-title>Nola Criddle Foundations</v-card-title>
+            <v-card-text>
+              A big thanks to the Nola Criddle Foundation for your continued
+              sponsorship. If we didn’t have your sponsorship for the last 4
+              years we wouldn’t have been able to start building our special
+              retirement home for dogs.
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="6" md="4">
+          <v-card>
+            <v-img
+              :src="
+                require('@/assets/sponsors/kevin-mcnulty-dog-behaviourist.jpg')
+              "
+              height="200px"
+              alt="kevin-mcnulty-dog-behaviourist"
+            >
+            </v-img>
+            <v-card-title>Kevin McNulty Dog Behaviourist</v-card-title>
+            <v-card-text>
+              Kevin provides gentle, professional training to dogs in the Perth
+              Metropolitan area. Call at 0416 791 195
+            </v-card-text>
+            <v-card-actions>
+              <v-btn
+                href="https://www.facebook.com/k9trainingandcommunication/"
+                text
+                rel="noreferrer"
+              >
+                Visit
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
   </div>
 </template>
 
@@ -219,3 +221,25 @@ export default {
   },
 }
 </script>
+
+<style lang="scss">
+.full-width {
+  background-color: rgb(247, 247, 247);
+  width: 100vw;
+  position: relative;
+  margin-left: -50vw;
+  left: 50%;
+  margin-bottom: -25px;
+  margin-top: -90px;
+}
+#regular {
+  width: 80%;
+  margin-left: 10%;
+}
+.top {
+  padding-top: 50px;
+}
+.bottom {
+  padding-bottom: 50px;
+}
+</style>
