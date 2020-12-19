f35f140d0945056fb567410d61bff5b196a3c4c5
Gradialas <anshulkotha@yahoo.co.in>
2020-12-19 14:24:54 +0800

    Added contents to the health vaccine page using Grid System. Any feedback would be helpful

---
+++ client/pages/canine-vaccination-protocols.vue
@@ -0,0 +1,262 @@
+<template>
+  <v-container>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="10" md="8" lg="8" xl="8">
+        <v-card elevation="0">
+          <div style="text-align: center">
+            <h1>Canine Vaccination Protocols</h1>
+            <h2>Don’t over vaccinate your dog!</h2>
+            <h3>
+              Read or
+              <a
+                href="https://veteducation.wistia.com/medias/eenuknz91n?inf_contact_key=ef4fd88423412625a8b3d1755039f9d31cdc4c6ba9b8e1ed1a6ed5ee73307bba"
+                >listen to the webinar
+              </a>
+              on Canine Vaccination by Professor Michael Day
+            </h3>
+          </div>
+          <v-img
+            :src="require('@/assets/dog-vaccination-terrier-110687510-011.jpg')"
+            alt="Dog Vaccination"
+            max-height="500"
+            max-width="1920"
+          />
+          <p style="font-size: 12px; text-align: center">
+            School of Veterinary Sciences, University of Bristol, United Kingdom
+            (BSc BVMS (Hons) PhD DSc Dipl ECVP FASM FRCPath FRCVS)
+          </p>
+          <br />
+
+          <h2>Global Canine Vaccination Guidelines</h2>
+          <br class="small" />
+          <p>
+            There are two sets of canine vaccination guidelines available: those
+            produced by the American Animal Hospital Association [1] and those
+            from the WSAVA VGG [2]. The fundamental principle of both sets of
+            guidelines, as encapsulated by the VGG, is that ‘we should aim to
+            vaccinate every animal with core vaccines and to vaccinate each
+            individual less frequently by only giving non-core vaccines that are
+            necessary for that animal’.
+          </p>
+          <p>
+            The WSAVA guidelines suggest that we should aim to vaccinate MORE
+            animals. This relates to the phenomenon of ‘herd immunity’. Herd
+            immunity suggests that where at least 75% of a herd of animals is
+            vaccinated, it is difficult for an infectious disease outbreak to
+            occur. The ‘herd’ for a small animal practitioner is the population
+            of dogs living within his or her practice area – and our aim should
+            be to have as many of these animals vaccinated as possible, in order
+            to reduce the chances of disease outbreak in the herd. This is
+            particularly important in the context of canine rabies. Where a mass
+            vaccination campaign results in at least 70% of the dog population
+            receiving vaccine, there is marked impact on the prevalence of
+            canine and human rabies.
+          </p>
+          <p>
+            In order to apply the principles of vaccination guidelines, it is
+            firstly necessary to understand the definitions of ‘core’ and
+            ‘non-core’ vaccines. CORE vaccines are those that all animals should
+            receive to protect them against diseases of global significance or
+            where legislation may dictate [i.e. canine rabies]. The use of
+            NON-CORE vaccines is dictated by geographical location, lifestyle
+            and exposure risk. Some vaccines are NOT RECOMMENDED because there
+            is little scientific justification for their use.
+          </p>
+          <p>
+            For dogs, the core vaccines are those that protect against canine
+            distemper virus (CDV), canine adenovirus (CAV) and canine
+            parvovirus-2 (CPV). In any country in which rabies is an endemic
+            disease, then rabies vaccination is also considered core for dogs.
+            Non-core canine vaccines include those that protect against
+            leptospirosis, canine parainfluenza virus (CPi), Bordetella
+            bronchiseptica and Borrelia. Canine coronavirus (CCV) vaccine is not
+            recommended as there is little evidence that CCV is a primary
+            enteric pathogen or that the vaccine can protect against such
+            infection.
+          </p>
+          <p>
+            WSAVA guidelines provide generic advice to practitioners, but it is
+            impossible to ensure that the guidelines are tailored to best fit
+            the local situation in each of the 78 WSAVA member countries. The
+            VGG encourages national associations to adapt and modify the
+            guidelines for local use where appropriate. This process might
+            involve altering the classification of a vaccine. For example, in
+            the UK, Leptospira vaccine is considered core for the dog and
+            attempts are now being made to provide data that define disease
+            prevalence [3].
+          </p>
+          <br />
+
+          <h2>Core Vaccination of Puppies</h2>
+          <br class="small" />
+          <p>
+            The vaccination of puppies is determined by the transfer of
+            maternally-derived antibody (MDA) from the bitch in colostrum. This
+            antibody is crucial for protection of the pup during early life, but
+            simultaneously blocks the endogenous immune response of the puppy to
+            vaccination. Canine immunoglobulin has a half life of around 11 days
+            and there is progressive decline in MDA concentrations in pups over
+            the first weeks of life. The ‘window of susceptibility’ occurs when
+            there is no longer sufficient maternal antibody to provide full
+            protection from infectious disease, but where sufficient antibody
+            remains to block the ability of the pup to make its own immune
+            response to modified live virus (MLV) vaccine. Traditionally, this
+            window has been taken to occur at between 8 – 10 weeks of age, but
+            new evidence shows that higher titre vaccines increase maternal
+            antibody concentrations leading to persistence of MDA for longer
+            periods of time. Studies have now shown that around 1 in every 10
+            puppies has ‘blocking’ levels of MDA at 12 weeks of age. For this
+            reason, vaccination guidelines now recommend that puppy vaccination
+            (with MLV core vaccines) starts at 8 – 9 weeks of age, with a second
+            vaccine 3 – 4 weeks later and a third vaccine given between 14 – 16
+            weeks of age (preferably at 16 weeks), to ensure that all pups have
+            received at least one dose of vaccine in the absence of interfering
+            MDA. A 12 month booster vaccine (either at 12 months of age or 12
+            months after the 16 week vaccine) is given to ensure full immunity
+            develops. Where rabies is endemic, pups should receive 1 dose of
+            vaccine at 12 weeks of age, but the VGG suggests that in a high-risk
+            situation, a second dose of vaccine may be given 2 &#8211; 4 weeks
+            later.
+          </p>
+          <br />
+
+          <h2>Core Vaccination of Adult Dogs</h2>
+          <br class="small" />
+          <p>
+            For adult dogs, MLV core vaccines should be given no more frequently
+            than every 3 years. For CDV, CAV, CPV there is excellent correlation
+            between the presence of serum antibody (virus neutralization test or
+            haemagglutination inhibition test) and protection from challenge
+            with infectious virus. There are extensive data showing that
+            protective antibody persists in adult dogs, even when they have only
+            been vaccinated as puppies up to 14 years previously [4, 5]. More
+            importantly there are data that underpin the legal registration of
+            canine core MLV vaccines for either 3 or 4 years, based on challenge
+            studies that show that vaccinated dogs resist infection for that
+            minimum period after vaccination (the minimum duration of immunity,
+            DOI). Other experimental data show that dogs vaccinated as puppies
+            only are protected from live virus challenge with CDV and CPV at 9
+            years of age [4]. On this basis, most of the internationally
+            produced canine MLV core vaccines used in the USA, Europe and
+            elsewhere now have a licensed minimum DOI of either 3 or 4 years.
+          </p>
+          <p>
+            This is also true for most of the internationally produced
+            adjuvanted killed rabies vaccines that may legally be given every 3
+            years rather than annually. Where such products are available with a
+            3-year licensed DOI, but governmental legislation insists on annual
+            rabies vaccination, it is beholden on the veterinary profession to
+            lobby for changes to the law in order to prevent unnecessary
+            revaccination of adult dogs. For example, in the USA, state laws
+            changed gradually, such that now every US state stipulates triennial
+            revaccination of dogs against rabies.
+          </p>
+          <p>
+            The ideal core revaccination schedule for adult dogs would therefore
+            be revaccination every third year with CDV, CAV, CPV and rabies.
+            Where rabies revaccination is still required annually, the schedule
+            might be CDV, CAV and CPV triennially and rabies annually. Triennial
+            revaccination reduces the number of unnecessary vaccines given to
+            adult dogs and therefore reduces the changes of adverse reactions.
+          </p>
+          <br />
+
+          <h2>Non-core Vaccination</h2>
+          <br class="small" />
+          <p>
+            Non-core vaccines should be selected for the individual dog based on
+            assessment of that particular animal’s risk of exposure to the
+            disease and assessment of the benefits of vaccination to that pet
+            versus the risk of adverse reaction. Decision making for non-core
+            vaccines would be facilitated by having available good quality data
+            and disease distribution maps related to small animal infectious
+            diseases. Unfortunately, with the exception of rabies in the USA and
+            Europe, such distribution maps are not widely available. Some
+            national schemes have been developed by industry or academic groups
+            which allow practitioners to input cases of particular infectious
+            diseases into a database that presents the information as disease
+            distribution maps.
+          </p>
+          <p>
+            Monitoring the distribution and evolution of infectious diseases is
+            an important part of vaccinology. An excellent example is canine
+            leptospirosis, which has recently attracted much research interest
+            as the importance of particular serovars in causing canine disease
+            in different geographical locations is determined. This new
+            knowledge has led to the introduction of tetravalent canine
+            leptospirosis vaccines in the US and Europe; the antigenic
+            composition of which is related to the prevalence of serovars in
+            each location. Similarly, in some countries (e.g. the USA and Korea)
+            vaccines are available to protect against strains of canine
+            influenza virus (CIV). This infection remains an issue for dogs that
+            are intensively kenneled and transported (e.g. racing greyhounds),
+            but the CIV vaccine would not be recommended for general use among
+            pet dogs.
+          </p>
+          <p>
+            Non-core vaccines may be included into the puppy vaccination
+            schedule if dictated by risk assessment. Intranasal vaccines
+            protecting against some elements of the canine infectious
+            respiratory disease complex (i.e. CPi and Bordetella bronchiseptica)
+            might be used as early as four weeks of age (the VGG currently also
+            suggests a second vaccine given 4 weeks later). The VGG recommends
+            that where Leptospira vaccines are used in puppies (particularly
+            adjuvanted bacterins) that these be given after the MLV core
+            vaccines (e.g. at 18 and 22 weeks of age).
+          </p>
+          <p>
+            A major difference between MLV core vaccines and all of the non-core
+            vaccines is that noncore vaccines (where used) require annual
+            boosters as their DOI is no greater than 12–18 months. Adult dogs
+            given non-core vaccines must therefore receive these annually. In
+            many situations therefore, adult dogs receive ‘annual
+            revaccination’, but just with fewer components than might have been
+            used in the past.
+          </p>
+          <br />
+
+          <h2>The Annual Health Check</h2>
+          <br class="small" />
+          <p>
+            All aspects of vaccination should fall under an annual health check
+            programme that reduces the emphasis on vaccination as a reason for
+            visiting the practice and considers holistically the overall health
+            and wellbeing of the pet. A discussion about which vaccines (or
+            serological tests) might be offered in any one year is just one part
+            of the annual health check. The importance of vaccination can be
+            reinforced by using the VGG fact sheets. Vaccination (or serology)
+            should be appropriately invoiced so emphasis is placed on the
+            professional consultation.
+          </p>
+          <br />
+
+          <p><strong>References</strong></p>
+          <ol>
+            <li>
+              Welborn LV, DeVries JG, Ford R et al. 2011 AAHA canine vaccination
+              guidelines. J Am Anim Hosp Assoc. 2011. 47: 5.
+            </li>
+            <li>
+              Day MJ, Horzinek M, Schultz RD. Guidelines for the vaccination of
+              dogs and cats. J Small Anim Pract. 2010. 51: 338-56.
+            </li>
+            <li>
+              Ball C, Dawson S, Williams N. Leptospira cases and vaccination
+              habits within UK vetvisiting dogs. Vet Rec. 2014. 174: 278.
+            </li>
+            <li>
+              Schultz RD, Thiel R, Mukhtar E et al. Age and long-term protective
+              immunity in dogs and cats. J Comp Pathol. 2010. 142: S102-08.
+            </li>
+            <li>
+              Mitchell SA, Zwinjnenberg RJ, Huang J et al. Duration of
+              serological response to canine parvovirus-type 2, canine distemper
+              virus, canine adenovirus type 1 and canine parainfluenza virus in
+              client-owned dogs in Australia. Aust Vet J. 2012. 90: 468-73.
+            </li>
+          </ol>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
