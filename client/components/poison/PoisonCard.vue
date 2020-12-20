0259d703d519d601fdd51704eef147cae9cba492
Han <tsinchooi@gmail.com>
2020-12-20 12:17:18 +0800

    Harmful Plant Page

---
+++ client/components/poison/PoisonCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card class="mx-auto elevation-0" width="80%">
+    <v-img :src="imagelink" width="80%" class="mx-auto"></v-img>
+    <v-card-title class="justify-center">{{ cardtitle }}</v-card-title>
+    <v-card-text align="center" v-html="carddescription">
+      {{ carddescription }}
+    </v-card-text>
+    <v-card-title class="justify-center"> Symptoms if ingested: </v-card-title>
+    <v-card-text align="center" v-html="symptoms">
+      {{ symptoms }}
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'PoisonCard',
+  props: {
+    imagelink: {
+      type: String,
+      default: '',
+    },
+    cardtitle: {
+      type: String,
+      default: '',
+    },
+    carddescription: {
+      type: String,
+      default: '',
+    },
+    symptoms: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
