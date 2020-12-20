51743e183ddfc211d40768b0bc6b40e6492b7a17
Han <tsinchooi@gmail.com>
2020-12-20 12:28:28 +0800

    Drafted Contact Page

---
+++ client/components/contact/ContactForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-form ref="form" v-model="valid" lazy-validation>
+    <h4>First Name</h4>
+    <v-text-field
+      v-model="firstname"
+      :counter="20"
+      :rules="nameRules"
+      required
+    ></v-text-field>
+    <h4>Last Name</h4>
+    <v-text-field
+      v-model="lastname"
+      :counter="20"
+      :rules="nameRules"
+      required
+    ></v-text-field>
+    <h4>Email</h4>
+    <v-text-field v-model="email" :rules="emailRules" required></v-text-field>
+    <h4>Mobile Number</h4>
+    <v-text-field
+      v-model="phoneNumber"
+      :counter="7"
+      :error-messages="errors"
+      required
+    ></v-text-field>
+    <h4>Message</h4>
+    <v-text-field
+      v-model="message"
+      :counter="500"
+      :error-messages="errors"
+      required
+    ></v-text-field>
+
+    <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">
+      Send
+    </v-btn>
+
+    <v-btn color="error" class="mr-4" @click="reset">Reset Form</v-btn>
+
+    <v-btn color="warning" @click="resetValidation">Reset Validation</v-btn>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: 'ContactForm',
+}
+</script>
---
+++ client/pages/contact.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container style="background-color: light-blue" color="black">
+    <v-container style="background-color: light-blue" color="black">
+      <ContactForm />
+    </v-container>
+  </v-container>
+</template>
+
+<script>
+import ContactForm from '~/components/contact/ContactForm.vue'
+
+export default {
+  components: {
+    ContactForm,
+  },
+}
+</script>
