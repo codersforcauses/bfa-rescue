3012c46df8a42423c6fc21e5148f3a5120d33958
Drewbi <drewalexander986@gmail.com>
2021-01-16 14:28:38 +0800

    remove text variable

--- client/components/health/Card.vue
+++ client/components/health/Card.vue
@@ -1,12 +1,14 @@
 <template>
-  <v-card class="flex flex-column pb-10 healthcard">
-    <v-img :src="imagelink" height="250px"></v-img>
-    <v-card-title>{{ cardtitle }}</v-card-title>
-    <v-card-text class="flex">{{ carddescription }}</v-card-text>
-    <v-card-actions class="cardactions">
-      <v-btn text color="#1badbe" :href="cardlink">Learn More</v-btn>
-    </v-card-actions>
-  </v-card>
+  <NuxtLink :to="cardlink">
+    <v-card class="flex flex-column pb-10 healthcard">
+      <v-img :src="imagelink" height="250px"></v-img>
+      <v-card-title>{{ cardtitle }}</v-card-title>
+      <v-card-text class="flex">{{ carddescription }}</v-card-text>
+      <v-card-actions class="cardactions">
+        <v-btn text color="#1badbe" :href="cardlink">Learn More</v-btn>
+      </v-card-actions>
+    </v-card>
+  </NuxtLink>
 </template>
 
 <script>
