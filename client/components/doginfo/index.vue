d400c78c0929889cd3fb6afcd608d35aba561c57
ivy0305 <64839317+ivy0305@users.noreply.github.com>
2021-01-25 17:09:47 +0800

    dog info page

---
+++ client/components/doginfo/index.vue
@@ -0,0 +1,116 @@
+<template>
+  <v-row>
+    <v-col cols="12" sm="6">
+      <v-img :src="imagelink" max-height="550"></v-img>
+    </v-col>
+    <v-col cols="12" sm="6">
+      <h3 class="text-center" style="font-size: 20px">Meet {{ name }}!</h3>
+      <v-list>
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon>mdi-calendar-range</v-icon>
+          </v-list-item-action>
+          <v-list-item-content><b>Age:</b>&nbsp;{{ age }}</v-list-item-content>
+        </v-list-item>
+        <v-divider inset></v-divider>
+
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon>mdi-dna</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <b>Breed:</b>&nbsp;{{ breed }}
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider inset></v-divider>
+
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon v-if="sex == 'Male'">mdi-gender-male</v-icon>
+            <v-icon v-else>mdi-gender-female</v-icon>
+          </v-list-item-action>
+          <v-list-item-content><b>Sex:</b>&nbsp;{{ sex }}</v-list-item-content>
+        </v-list-item>
+        <v-divider inset></v-divider>
+
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon>mdi-dog</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <b>Size:</b>&nbsp;{{ size }}
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider inset></v-divider>
+
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon>mdi-cash-usd-outline</v-icon>
+          </v-list-item-action>
+          <v-list-item-content><b>Fee:</b>&nbsp;${{ fee }}</v-list-item-content>
+        </v-list-item>
+        <v-divider inset></v-divider>
+
+        <v-list-item>
+          <v-list-item-content>{{ description }}</v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <div class="text-center">
+        <v-btn class="ma-2" outlined color="#1badbe" href="/adoption-form">
+          Apply to adopt {{ name }}
+        </v-btn>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'DogInfo',
+  props: {
+    imagelink: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+    age: {
+      type: String,
+      default: '',
+    },
+    breed: {
+      type: String,
+      default: '',
+    },
+    sex: {
+      type: String,
+      default: '',
+    },
+    size: {
+      type: String,
+      default: '',
+    },
+    fee: {
+      type: Number,
+      default: 0,
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style scoped>
+.v-icon {
+  color: #1badbe;
+}
+
+b {
+  display: inline !important;
+  flex: none;
+}
+</style>
---
+++ client/pages/dog/_id.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-container>
+    <Doginfo
+      :imagelink="selectedDog.imagelink"
+      :name="selectedDog.name"
+      :age="selectedDog.age"
+      :breed="selectedDog.breed"
+      :sex="selectedDog.sex"
+      :size="selectedDog.size"
+      :fee="selectedDog.fee"
+      :description="selectedDog.description"
+    ></Doginfo>
+  </v-container>
+</template>
+
+<script>
+import Doginfo from '~/components/doginfo'
+
+export default {
+  components: {
+    Doginfo,
+  },
+  data() {
+    return {
+      dogs: [
+        {
+          _id: '1',
+          imagelink:
+            'https://bfarescue.com.au/wp-content/uploads/2020/11/Harley-standing-650x486.jpg',
+          name: 'Harley',
+          age: '8 yrs',
+          breed: 'Border Collie',
+          sex: 'Male',
+          size: 'Medium',
+          fee: 500,
+          description:
+            'What a lovely, gorgeous & energetic gentleman! Meet Harley, a superb black & white border collie male! That tuxedo he is wearing goes with his good boy nature! He has been in a home with another dog, cats and kids, so he has seen it all. Now he is looking for the new home with a new doggie friend of his own, through no fault of his! He is sociable and will only be placed with a family that has another dog already.',
+        },
+        {
+          _id: '2',
+          imagelink:
+            'https://bfarescue.com.au/wp-content/uploads/2020/11/Mischief-head-and-shoulders.png',
+          name: 'Mischief',
+          age: '7 yrs',
+          breed: 'German Shepherd',
+          sex: 'Female',
+          size: 'Large',
+          fee: 550,
+          description:
+            'This beautiful, dignified matron of seven years is somewhat misnamed! Though she is a lively and friendly , used to older kids and cats, she is also loving and pretty good in learning to mind her manners. Mischief won’t stand on her dignity, though! Give her a chance and she is off for a romp! She will need someone who is dedicated to obedience training, as she is a big girl, but she is willing to learn. Hold on tight!',
+        },
+      ],
+    }
+  },
+  computed: {
+    selectedDog() {
+      return this.dogs.find((dog) => dog._id === this.$route.params.id)
+    },
+  },
+}
+</script>
