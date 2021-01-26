35bd1530bf2ff60e9998767d5ad90deab32b6ff2
b053040009@gmail.com <b053040009@gmail.com>
2021-01-26 21:46:16 +0800

    add mongodb migration and create first example migration

---
+++ server/migrations/20210126103553-add-user.js
@@ -0,0 +1,26 @@
+module.exports = {
+  async up (db, client) {
+    // TODO write your migration here.
+    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
+    // Example:
+    await db.collection('users').insertOne({
+      firstname: 'Test',
+      lastname: 'User',
+      email: 'test@bfa.com.au',
+      password: 'admin456',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
+  },
+
+  async down (db, client) {
+    // TODO write the statements to rollback your migration (if possible)
+    // Example:
+    await db.collection('users').deleteOne({
+      firstname: 'Test',
+      lastname: 'User',
+      email: 'test@bfa.com.au',
+      password: 'admin456'
+    })
+  }
+}
