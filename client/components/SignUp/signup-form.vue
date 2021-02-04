8756bf327fa3505043dc7405a1de78835b6bf2b7
Han <tsinchooi@gmail.com>
2021-02-04 14:42:43 +0800

    Ready to be merged

--- client/components/SignUp/signup-form.vue
+++ client/components/SignUp/signup-form.vue
@@ -1,78 +1,83 @@
 <template>
   <v-sheet tile elevation="0" color="white">
     <v-container class="py-16">
-      <h2 style="text-align: center">Sign Up</h2>
-      <v-form ref="form" v-model="valid" lazy-validation>
-        <v-text-field
-          v-model="firstname"
-          clearable
-          outlined
-          solo
-          :rules="nameRules"
-          required
-          label="First Name"
-        >
-        </v-text-field>
-        <v-text-field
-          v-model="lastname"
-          outlined
-          :rules="nameRules"
-          solo
-          required
-          label="Last Name"
-        >
-        </v-text-field>
-        <v-text-field
-          v-model="email"
-          outlined
-          :rules="emailRules"
-          solo
-          required
-          label="Email"
-        >
-        </v-text-field>
-        <v-text-field
-          v-model="mobileNumber"
-          outlined
-          solo
-          :rules="mobileRules"
-          required
-          label="Mobile Number"
-        >
-        </v-text-field>
-        <v-text-field
-          v-model="password"
-          clearable
-          outlined
-          solo
-          label="Password"
-          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-          :rules="PWRules"
-          :type="show1 ? 'text' : 'password'"
-          @click:append="show1 = !show1"
-        >
-        </v-text-field>
-        <v-text-field
-          v-model="password2"
-          clearable
-          outlined
-          solo
-          label="Confirm Password"
-          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-          :rules="PWRules2"
-          :type="show1 ? 'text' : 'password'"
-          @click:append="show1 = !show1"
-        >
-        </v-text-field>
-        <v-btn
-          :disabled="!valid"
-          color="primary"
-          class="mr-4"
-          @click="validate"
-        >
-          Send
-        </v-btn>
-      </v-form>
+      <v-row>
+        <v-col cols="6" class="mx-auto">
+          <h2 style="text-align: center; margin-bottom: 2rem">Sign Up</h2>
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field
+              v-model="firstname"
+              clearable
+              outlined
+              solo
+              :rules="nameRules"
+              required
+              label="First Name"
+            >
+            </v-text-field>
+            <v-text-field
+              v-model="lastname"
+              outlined
+              :rules="nameRules"
+              solo
+              required
+              label="Last Name"
+            >
+            </v-text-field>
+            <v-text-field
+              v-model="email"
+              outlined
+              :rules="emailRules"
+              solo
+              required
+              label="Email"
+            >
+            </v-text-field>
+            <v-text-field
+              v-model="mobileNumber"
+              outlined
+              solo
+              :rules="mobileRules"
+              required
+              label="Mobile Number"
+            >
+            </v-text-field>
+            <v-text-field
+              v-model="password"
+              clearable
+              outlined
+              solo
+              label="Password"
+              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="PWRules"
+              :type="show1 ? 'text' : 'password'"
+              @click:append="show1 = !show1"
+            >
+            </v-text-field>
+            <v-text-field
+              v-model="password2"
+              clearable
+              outlined
+              solo
+              lazy-validation
+              label="Confirm Password"
+              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="[PWRules2, compare_password]"
+              :type="show1 ? 'text' : 'password'"
+              @click:append="show1 = !show1"
+            >
+            </v-text-field>
+            <v-btn
+              :disabled="!valid"
+              color="primary"
+              class="mr-4"
+              @click="validate"
+            >
+              Send
+            </v-btn>
+          </v-form>
+        </v-col>
+      </v-row>
     </v-container>
   </v-sheet>
 </template>
@@ -80,6 +85,11 @@
 <script>
 export default {
   name: 'ContactForm',
+  computed: {
+    compare_password() {
+      return this.password === this.password2 || 'Password must match'
+    },
+  },
   data: () => ({
     show1: false,
     valid: true,
@@ -101,7 +111,7 @@ export default {
     PWRules: [(v) => !!v || 'Password required'],
     password: '',
     password2: '',
-    PWRules2: [(v) => !!v || 'Password required'],
+    PWRules2: (v) => !!v || 'Password required',
   }),
   methods: {
     validate() {
