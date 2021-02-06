8d6348c02fc454088ead45dd34e6b8791cde362b
Aviciena Santoso <avi.santoso@gmail.com>
2021-02-07 07:12:32 +0800

    add basic validation to Users, change schema to fit Register pr

--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,37 @@
 const mongoose = require('mongoose')
+const validator = require('validator').default
 
 const userSchema = new mongoose.Schema(
   {
-    username: {
+    firstName: {
       type: String,
-      unique: true,
       required: true
+    },
+    lastName: {
+      type: String
     },
     email: {
       type: String,
-      required: true
+      unique: true,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return validator.isEmail(validator.normalizeEmail(value))
+        },
+        message: (props) => `${props.value} is not a valid email address.`
+      }
+    },
+    mobileNumber: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return validator.isMobilePhone(value, 'en-AU')
+        },
+        message: (props) =>
+          `${props.value} is not a valid Australian mobile number.`
+      }
     },
     password: {
       type: String,
