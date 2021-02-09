9ba8f151cfe1e1fe728a6d4f658d567ac70c332f
Aviciena Santoso <avi.santoso@gmail.com>
2021-02-09 12:28:22 +0800

    add initial save test and phone number normalization

--- server/models/User.js
+++ server/models/User.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose')
 const validator = require('validator').default
 const bcrypt = require('bcryptjs')
 
+// Google's phone number parsing and validation library
+const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance()
+const PNF = require('google-libphonenumber').PhoneNumberFormat
+
 const userSchema = new mongoose.Schema(
   {
     firstName: {
@@ -46,21 +50,37 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 )
 
+// Normalizing inputs middleware
 userSchema.pre('save', async function (next) {
-  if (this.password && this.isModified('password')) {
-    const salt = await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password, salt)
+  try {
+    // Hash password (so we don't have to work with hashing during tests and seeding)
+    if (this.password && this.isModified('password')) {
+      const salt = await bcrypt.genSalt(10)
+      this.password = await bcrypt.hash(this.password, salt)
+    }
+
+    // Normalize email
+    if (this.email && this.isModified('email')) {
+      this.email = validator.normalizeEmail(this.email)
+    }
+
+    // Normalize mobile number
+    if (this.mobileNumber && this.isModified('mobileNumber')) {
+      try {
+        const ausNumber = phoneUtil.parse(this.mobileNumber, 'AU')
+        this.mobileNumber = phoneUtil.format(ausNumber, PNF.INTERNATIONAL)
+      } catch (err) {
+        console.error(
+          `Cannot convert mobile number ${this.mobileNumber} to international format.`,
+          err
+        )
+      }
+    }
+
+    next()
+  } catch (error) {
+    console.error(error)
   }
-
-  next()
-})
-
-userSchema.pre('save', async function (next) {
-  if (this.email && this.isModified('email')) {
-    this.email = validator.normalizeEmail(this.email)
-  }
-
-  next()
 })
 
 const User = mongoose.model('User', userSchema)
--- server/models/__tests__/User.test.js
+++ server/models/__tests__/User.test.js
@@ -3,19 +3,41 @@ const User = require('../User')
 
 describe('User.js', () => {
   let connection
+
   beforeAll(async () => {
-    connection = await mongoose.connect(
-      global.__MONGO_URI__,
-      { useNewUrlParser: true, useCreateIndex: true },
-      (err) => {
-        if (err) {
-          console.error(err)
-          process.exit(1)
-        }
-      }
-    )
+    connection = await mongoose.connect(global.__MONGO_URI__, {
+      useNewUrlParser: true,
+      useCreateIndex: true
+    })
   })
+
   afterAll(async () => {
     await connection.close()
+  })
+
+  test('should be able to create and save a new user with a hashed password', async () => {
+    const userData = {
+      firstName: 'Avi',
+      lastName: 'Santoso',
+      email: 'testemail@gmail.com',
+      mobileNumber: '0412341234',
+      password: 'SuperSecretPassword321'
+    }
+
+    // Alternatively, use User.create(). However, this will cause bcrypt to linger.
+    // The reason for this is Mongoose assuming you want to push bulk documents.
+    // It keeps the random number generator service open for efficiency.
+    const unsavedUser = new User(userData)
+    const newUser = await unsavedUser.save()
+
+    expect(newUser.firstName).toBe(userData.firstName)
+    expect(newUser.lastName).toBe(userData.lastName)
+    expect(newUser.email).toBe(userData.email)
+
+    // Normalized to international mobile number on save.
+    expect(newUser.mobileNumber).toBe('+61 412 341 234')
+
+    // Should not be the same password due to hashing.
+    expect(newUser.password).not.toBe(userData.password)
   })
 })
