bcfba28d3168b406033b63a06b55217486cfddc2
Aviciena Santoso <avi.santoso@gmail.com>
2021-02-10 18:39:23 +0800

    wip seeding

--- server/seeder.js
+++ server/seeder.js
@@ -1,1 +1,17 @@
+const User = require('./models/User')
 
+async function main() {
+  try {
+    const adminUser = new User({
+      firstName: 'Admin',
+      email: 'admin@bfarescue.com.au',
+      mobileNumber: '0412341234',
+      password: 'SuperSecretPassword321'
+    })
+    await adminUser.save()
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+main()
