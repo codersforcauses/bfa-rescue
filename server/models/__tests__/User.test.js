c74bbfa8ba96ef9a2bbfc9d7eecaf458ac14424c
Aviciena Santoso <avi.santoso@gmail.com>
2021-02-10 18:25:40 +0800

    add test for invalid email address

--- server/models/__tests__/User.test.js
+++ server/models/__tests__/User.test.js
@@ -15,15 +15,15 @@ describe('User.js', () => {
     await connection.close()
   })
 
-  test('should be able to create and save a new user with a hashed password', async () => {
-    const userData = {
-      firstName: 'Avi',
-      lastName: 'Santoso',
-      email: 'testemail@gmail.com',
-      mobileNumber: '0412341234',
-      password: 'SuperSecretPassword321'
-    }
+  const userData = {
+    firstName: 'Avi',
+    lastName: 'Santoso',
+    email: 'testemail@gmail.com',
+    mobileNumber: '0412341234',
+    password: 'SuperSecretPassword321'
+  }
 
+  test('should be able to create and save a new user with a hashed password', async () => {
     // Alternatively, use User.create(). However, this will cause bcrypt to linger.
     // The reason for this is Mongoose assuming you want to push bulk documents.
     // It keeps the random number generator service open for efficiency.
@@ -39,5 +39,13 @@ describe('User.js', () => {
 
     // Should not be the same password due to hashing.
     expect(newUser.password).not.toBe(userData.password)
+  })
+
+  test('should throw when adding of users with an invalid email address', async () => {
+    expect(async () => {
+      const dataWithInvalidEmail = { ...userData, email: '1234' }
+      const unsavedUser = new User(dataWithInvalidEmail)
+      await unsavedUser.save()
+    }).rejects.toThrow('not a valid email address')
   })
 })
