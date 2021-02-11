29a4b23a8b31ff9119dc5dfe348abbf6c194ef45
Aviciena Santoso <avi.santoso@gmail.com>
2021-02-11 11:07:31 +0800

    add common email address test

--- server/__tests__/models/User.test.js
+++ server/__tests__/models/User.test.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
-const User = require('../User')
+const { User } = require('../../models')
 
-describe('User.js', () => {
+describe('User Model', () => {
   let connection
 
   beforeAll(async () => {
@@ -19,26 +19,46 @@ describe('User.js', () => {
     firstName: 'Avi',
     lastName: 'Santoso',
     email: 'testemail@gmail.com',
-    mobileNumber: '0412341234',
+    mobileNumber: '+61 412 341 234',
     password: 'SuperSecretPassword321'
   }
 
   test('should be able to create and save a new user with a hashed password', async () => {
-    // Alternatively, use User.create(). However, this will cause bcrypt to linger.
-    // The reason for this is Mongoose assuming you want to push bulk documents.
-    // It keeps the random number generator service open for efficiency.
+    // Alternatively, use User.create().
     const unsavedUser = new User(userData)
     const newUser = await unsavedUser.save()
 
     expect(newUser.firstName).toBe(userData.firstName)
     expect(newUser.lastName).toBe(userData.lastName)
     expect(newUser.email).toBe(userData.email)
-
-    // Normalized to international mobile number on save.
-    expect(newUser.mobileNumber).toBe('+61 412 341 234')
+    expect(newUser.mobileNumber).toBe(userData.mobileNumber)
 
     // Should not be the same password due to hashing.
     expect(newUser.password).not.toBe(userData.password)
+  })
+
+  test('should normalize email addresses for users', async () => {
+    const email = 'avi.santoso@gmail.com'
+    const normalizedEmail = 'avisantoso@gmail.com'
+
+    const unsavedUser = new User({ ...userData, email })
+    const newUser = await unsavedUser.save()
+
+    expect(newUser.email).toBe(normalizedEmail)
+  })
+
+  test('should not allow creating multiple users with the same email address', async () => {
+    // Note, emailOne and emailTwo are considered the same email address for gmail.
+    const emailOne = 'avi.santoso@gmail.com'
+    const emailTwo = 'a.v.i.santoso@gmail.com'
+
+    const userOne = new User({ ...userData, email: emailOne })
+    await userOne.save()
+
+    expect(async () => {
+      const userTwo = new User({ ...userData, email: emailTwo })
+      await userTwo.save()
+    }).rejects.toThrow('same email address')
   })
 
   test('should throw when adding of users with an invalid email address', () => {
