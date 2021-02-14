d9120d11c5dc955e33e146ca130ad34f15561e70
Aviciena Santoso <avi.santoso@gmail.com>
2021-02-14 09:08:13 +0800

    fix User.test.js and add mobile number tests

--- server/__tests__/models/User.test.js
+++ server/__tests__/models/User.test.js
@@ -2,6 +2,14 @@ const mongoose = require('mongoose')
 
 const { User } = require('../../models')
 
+const sampleUser = {
+  firstName: 'Avi',
+  lastName: 'Santoso',
+  email: 'avisantoso@gmail.com',
+  mobileNumber: '+61 423 123 123',
+  password: 'SuperSecretPassword321'
+}
+
 describe('User Model', () => {
   let connection
 
@@ -17,90 +25,120 @@ describe('User Model', () => {
   })
 
   test('should be able to create and save a new user with a hashed password', async () => {
-    const sampleUser = {
-      firstName: 'Avi',
-      lastName: 'Santoso',
-      email: 'testemail@gmail.com',
-      mobileNumber: '+61 423 123 123',
-      password: 'SuperSecretPassword321'
-    }
-
     // Alternatively, use User.create().
-    const unsavedUser = new User(sampleUser)
-    const newUser = await unsavedUser.save()
+    const user = new User(sampleUser)
+    const savedUser = await user.save()
 
-    expect(newUser.firstName).toBe(sampleUser.firstName)
-    expect(newUser.lastName).toBe(sampleUser.lastName)
-    expect(newUser.email).toBe(sampleUser.email)
-    expect(newUser.mobileNumber).toBe(sampleUser.mobileNumber)
+    expect(savedUser.firstName).toBe(sampleUser.firstName)
+    expect(savedUser.lastName).toBe(sampleUser.lastName)
+    expect(savedUser.email).toBe(sampleUser.email)
+    expect(savedUser.mobileNumber).toBe(sampleUser.mobileNumber)
 
     // Should not be the same password due to hashing.
-    expect(newUser.password).not.toBe(sampleUser.password)
+    expect(savedUser.password).not.toBe(sampleUser.password)
+
+    await savedUser.delete()
   })
 
   test('should normalize email addresses for users', async () => {
-    const sampleUser = {
-      firstName: 'Avi',
-      lastName: 'Santoso',
-      email: 'normalize.test@gmail.com',
-      mobileNumber: '+61 423 123 124',
-      password: 'SuperSecretPassword321'
-    }
+    const email = 'avi.santoso@gmail.com'
+    const normalizedEmail = 'avisantoso@gmail.com'
 
-    const normalizedEmail = 'normalizetest@gmail.com'
-    const newUser = await User.create(sampleUser)
-    expect(newUser.email).toBe(normalizedEmail)
+    const user = new User({ ...sampleUser, email })
+    const savedUser = await user.save()
+
+    expect(savedUser.email).toBe(normalizedEmail)
+
+    await savedUser.delete()
   })
 
-  test('should not allow creating multiple users with the same email address', async () => {
-    // Note, emailOne and emailTwo are considered the same email address for gmail.
+  test('should normalize phone numbers for users', async () => {
+    const mobileNumber = '0430001999'
+    const normalizedMobileNumber = '+61 430 001 999'
+
+    const user = new User({ ...sampleUser, mobileNumber })
+    const savedUser = await user.save()
+
+    expect(savedUser.mobileNumber).toBe(normalizedMobileNumber)
+
+    await savedUser.delete()
+  })
+
+  test('should throw when creating multiple users with the same email address', async () => {
+    // Note, emailOne and emailTwo are considered the same email address.
 
     const sampleOne = {
-      firstName: 'Avi',
-      lastName: 'Santoso',
+      ...sampleUser,
       email: 'avi.santoso@gmail.com',
-      mobileNumber: '+61 423 123 125',
-      password: 'SuperSecretPassword321'
+      mobileNumber: '+61 423 123 125'
     }
 
     const sampleTwo = {
-      firstName: 'Avi',
-      lastName: 'Santoso',
+      ...sampleUser,
       email: 'a.v.i.santoso@gmail.com',
-      mobileNumber: '+61 423 123 126',
-      password: 'SuperSecretPassword321'
+      mobileNumber: '+61 423 123 125'
     }
 
+    const userOne = new User(sampleOne)
+    const userTwo = new User(sampleTwo)
+
     expect(async () => {
-      const userOne = await User.create(sampleOne)
-      const userTwo = await User.create(sampleTwo)
-      return { userOne, userTwo }
-    }).rejects.toThrow('same email address')
+      await userOne.save()
+      await userTwo.save()
+    }).rejects.toThrow('duplicate key error')
+
+    await userOne.delete()
+    await userTwo.delete()
   })
 
-  test('should throw when adding of users with an invalid email address', () => {
-    const invalidUser = {
-      firstName: 'Avi',
-      lastName: 'Santoso',
-      email: '1234',
-      mobileNumber: '+61 423 123 127',
-      password: 'SuperSecretPassword321'
+  test('should throw when creating multiple users with the same mobile number', async () => {
+    const sampleOne = {
+      ...sampleUser,
+      email: 'admin.one@gmail.com',
+      mobileNumber: '0430001999'
     }
+
+    const sampleTwo = {
+      ...sampleUser,
+      email: 'admin.two@gmail.com',
+      mobileNumber: '0430001999'
+    }
+
+    const userOne = new User(sampleOne)
+    const userTwo = new User(sampleTwo)
+
     expect(async () => {
-      await User.create(invalidUser)
+      await userOne.save()
+      await userTwo.save()
+    }).rejects.toThrow('duplicate key error')
+
+    await userOne.delete()
+    await userTwo.delete()
+  })
+
+  test('should throw when adding users with an invalid email address', async () => {
+    const user = new User({
+      ...sampleUser,
+      email: '1234'
+    })
+
+    expect(async () => {
+      await user.save()
     }).rejects.toThrow('not a valid email address')
+
+    await user.delete()
   })
 
-  test('should throw when adding of users with an invalid mobile number', () => {
-    const invalidUser = {
-      firstName: 'Avi',
-      lastName: 'Santoso',
-      email: 'throw.mobile.test@gmail.com',
-      mobileNumber: '1234',
-      password: 'SuperSecretPassword321'
-    }
+  test('should throw when adding users with an invalid mobile number', async () => {
+    const user = new User({
+      ...sampleUser,
+      mobileNumber: '1234'
+    })
+
     expect(async () => {
-      await User.create(invalidUser)
+      await user.save()
     }).rejects.toThrow('not a valid Australian mobile number')
+
+    await user.delete()
   })
 })
