fa1737e0f4521f2ffe7eab3b1a74b31b2fee7275
Frinze Erin Lapuz <frinzekt@gmail.com>
2021-04-10 14:07:44 +0800

    establish database connection

--- server/src/mongoose.js
+++ server/src/mongoose.js
@@ -4,7 +4,13 @@ const logger = require('./logger');
 module.exports = function (app) {
   mongoose.connect(
     app.get('mongodb'),
-    { useCreateIndex: true, useNewUrlParser: true }
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true, // Connection retry on first startup
+      poolSize: 10, // Maintain up to 10 socket connections
+    }
+
   ).catch(err => {
     logger.error(err);
     process.exit(1);
