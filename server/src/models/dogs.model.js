695ef1a4dcc2b2e4212d19c0ffb00fb3a2708047
Frinze Erin Lapuz <frinzekt@gmail.com>
2021-04-10 14:45:17 +0800

    dog model

--- server/src/models/dogs.model.js
+++ server/src/models/dogs.model.js
@@ -1,16 +1,53 @@
 // dogs-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
+const DescType = require('../types/desc.type');
+const NameType = require('../types/name.type');
+
 module.exports = function (app) {
   const modelName = 'dogs';
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
-  const schema = new Schema({
-    text: { type: String, required: true }
-  }, {
-    timestamps: true
-  });
+  const schema = new Schema(
+    {
+      name: NameType(),
+      age: {
+        year: {
+          type: Number,
+          required: true,
+          max: 50,
+          validate: Number.isInteger,
+        },
+        month: {
+          type: Number,
+          required: true,
+          min: 0,
+          max: 11,
+          validate: Number.isInteger,
+        },
+      },
+      breed: NameType(),
+      sex: {
+        type: String,
+        required: true,
+        enum: ['Male', 'Female'],
+      },
+      size: {
+        type: String,
+        required: true,
+        enum: ['Small', 'Medium', 'Large'],
+      },
+      description: DescType(),
+      isAvailable: {
+        type: Boolean,
+        required: true,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
 
   // This is necessary to avoid model compilation errors in watch mode
   // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
@@ -18,5 +55,4 @@ module.exports = function (app) {
     mongooseClient.deleteModel(modelName);
   }
   return mongooseClient.model(modelName, schema);
-  
 };
