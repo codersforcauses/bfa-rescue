ddaec46b0211ff52d9e7320373451ceed40dc226
Drewbi <drewalexander986@gmail.com>
2021-05-15 12:12:42 +0800

    remove sheet

--- client/components/login/LoginForm.vue
+++ client/components/login/LoginForm.vue
@@ -1,45 +1,43 @@
 <template>
-  <v-sheet tile elevation="0" color="white">
-    <v-container class="py-16">
-      <v-row>
-        <v-col cols="6" class="mx-auto">
-          <h2 class="text-center mb-5">Login</h2>
-          <v-form ref="form" v-model="valid" lazy-validation>
-            <v-text-field
-              v-model="loginID"
-              clearable
-              outlined
-              solo
-              :rules="IDRules"
-              required
-              label="Login"
-            >
-            </v-text-field>
-            <v-text-field
-              v-model="password"
-              clearable
-              outlined
-              solo
-              label="Password"
-              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-              :rules="PWRules"
-              :type="show1 ? 'text' : 'password'"
-              @click:append="show1 = !show1"
-            >
-            </v-text-field>
-            <v-btn
-              :disabled="!valid"
-              color="primary"
-              class="mr-4"
-              @click="validate"
-            >
-              Login
-            </v-btn>
-          </v-form>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-sheet>
+  <v-container class="py-16">
+    <v-row>
+      <v-col cols="6" class="mx-auto">
+        <h2 class="text-center mb-5">Login</h2>
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-text-field
+            v-model="loginID"
+            clearable
+            outlined
+            solo
+            :rules="IDRules"
+            required
+            label="Login"
+          >
+          </v-text-field>
+          <v-text-field
+            v-model="password"
+            clearable
+            outlined
+            solo
+            label="Password"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="PWRules"
+            :type="show1 ? 'text' : 'password'"
+            @click:append="show1 = !show1"
+          >
+          </v-text-field>
+          <v-btn
+            :disabled="!valid"
+            color="primary"
+            class="mr-4"
+            @click="validate"
+          >
+            Login
+          </v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
