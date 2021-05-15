23c5dd8d126842f89f3b8555405bd583c8e12629
Drewbi <drewalexander986@gmail.com>
2021-05-15 12:16:10 +0800

    remove uneeded component

--- client/pages/login.vue
+++ client/pages/login.vue
@@ -1,15 +1,72 @@
 <template>
-  <div>
-    <LoginForm />
-  </div>
+  <v-container class="py-16">
+    <v-row>
+      <v-col cols="12" sm="6" class="mx-auto">
+        <h2 class="text-center mb-5">Login</h2>
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-text-field
+            v-model="loginID"
+            clearable
+            outlined
+            solo
+            :rules="IDRules"
+            required
+            label="Login"
+          >
+          </v-text-field>
+          <v-text-field
+            v-model="password"
+            clearable
+            outlined
+            solo
+            label="Password"
+            :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="PWRules"
+            :type="showPass ? 'text' : 'password'"
+            @click:append="showPass = !showPass"
+          >
+          </v-text-field>
+          <v-btn
+            :disabled="!valid"
+            color="primary"
+            class="mr-4"
+            @click="validate"
+          >
+            Login
+          </v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import LoginForm from '~/components/login/LoginForm.vue'
-
 export default {
-  components: {
-    LoginForm,
+  name: 'LoginForm',
+  data: () => ({
+    showPass: false,
+    valid: true,
+    loginID: '',
+    IDRules: [(v) => !!v || 'Login required'],
+    PWRules: [(v) => !!v || 'Password required'],
+    password: '',
+  }),
+  methods: {
+    validate() {
+      this.$refs.form.validate()
+    },
+    reset() {
+      this.$refs.form.reset()
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation()
+    },
   },
 }
 </script>
+
+<style>
+.v-label {
+  font-weight: bold;
+}
+</style>
